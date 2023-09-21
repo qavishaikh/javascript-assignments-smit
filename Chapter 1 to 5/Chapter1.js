2744705fc731b2098dd4c86f27e395d0b674be26
Qavi <qavi.shaikh11@gmail.com>
2023-09-21 22:58:25 +0500

    JS Assignment Chapter 1 Alerts

---
+++ Chapter 1 to 5/Chapter1.js
@@ -0,0 +1,24 @@
+   
+//    Chapter No 1
+// Task 1
+
+//    var visitorName = prompt("What's your name?");
+    
+//    if (visitorName !== null && visitorName !== "") {
+//        alert("Hello, " + visitorName + "! Welcome to our website.");
+//    } else {
+//        alert("Hello, visitor! Welcome to our website.");
+//    }
+
+//    Task 2
+
+// var a = "Erro! Please Enter a valid Password"
+// alert(a)
+
+// // Task 3
+// var a = "Welcome to JS land \n Happy Coding!"
+// alert(a)
+
+// TAsk 5
+// var b = "Hello! i can run Js From Browser"
+// alert(b)
