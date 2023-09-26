5bde731556b78223ad4f3cc38e5af2d64bc09925
Qavi <qavi.shaikh11@gmail.com>
2023-09-26 23:08:24 +0500

    Chapter 5

---
+++ Chapter 1 to 5/Chapter5.js
@@ -0,0 +1,111 @@
+// Chapter 5 
+
+// Task 1 & Task 2
+// var num1 = +prompt("Enter Number 1")
+// var num2 = +prompt("Enter Number 2")
+// // var res = num1+num2
+// // var res = num1-num2
+// // var res = num1*num2
+// var res = num1/num2
+// document.write(res)
+
+// Task 3
+// // a. Declare a variable.
+// var myVariable;
+// // b. Show the value of the variable after declaration.
+// document.write("Value after variable declaration is: " + myVariable + "<br>");
+// // c. Initialize the variable with some number.
+// myVariable = 5;
+// // d. Show the initial value of the variable.
+// document.write("Initial value: " + myVariable + "<br>");
+// // e. Increment the variable.
+// myVariable++;
+// // f. Show the value after increment.
+// document.write("Value after increment is: " + myVariable + "<br>");
+// // g. Add 7 to the variable.
+// myVariable += 7;
+// // h. Show the value after addition.
+// document.write("Value after addition is: " + myVariable + "<br>");
+// // i. Decrement the variable.
+// myVariable--;
+// // j. Show the value after decrement.
+// document.write("Value after decrement is: " + myVariable + "<br>");
+// // k. Show the remainder after dividing the variable's value by 3.
+// var remainder = myVariable % 3;
+// // l. Show the remainder.
+// document.write("The remainder is: " + remainder + "<br>");
+
+// Task 4
+// var ticket = +prompt("Enter the price of Ticket")
+// var quantity = +prompt("Enter the Quantit of Ticket")
+// var totalCost = ticket*quantity
+// document.write(totalCost)
+
+// Task 5
+// var number = +(prompt("Enter a number for the Multiplication table:"));
+
+// if (!isNaN(number)) {
+//     document.write(`<h2>Multiplication Table for ${number}</h2>`);
+    
+//     // Display the multiplication table
+//     for (let i = 1; i <= 10; i++) {
+//         var result = number * i;
+//         document.write(`${number} x ${i} = ${result}<br>`);
+//     }
+// } else {
+//     document.write("Invalid input. Please enter a valid number.");
+// }
+
+//  Task 6
+// var celsiusTemperature = 28;
+
+// var fahrenheitTemperature = (celsiusTemperature * 9/5) + 32;
+// document.write(celsiusTemperature + "°C is " + fahrenheitTemperature + "°F");
+
+// var fahrenheitTemp = 82;
+
+// var celsiusTemp = (fahrenheitTemp - 32) * 5/9;
+// document.write(fahrenheitTemp + "°F is " + celsiusTemp + "°C");
+
+// Task 7
+        // var priceItem1 = +prompt("Enter the Price of Item 1");
+        // var priceItem2 = +prompt("Enter the Price of Item 2");
+        // var quantityItem1 = +prompt("Enter the Quantity of Item 1");
+        // var quantityItem2 = +prompt("Enter the Quantity of Item 2");
+
+        // var shippingCharges = +prompt("Enter the Shipping Charges");
+
+        // var totalCost = (priceItem1 * quantityItem1) + (priceItem2 * quantityItem2) + shippingCharges;
+
+        // // Display the receipt
+        // document.write("<h2>Receipt</h2>");
+        // document.write("<p>Item 1: " + priceItem1 + "<br>" + "Item 1 Quantity: " + quantityItem1 + "</p>");
+        // document.write("<p>Item 2: " + priceItem2 + " <br> " + "Item 2 Quantity: " + quantityItem2 + "</p>");
+        // document.write("<p>Shipping Charges: " + shippingCharges + "</p>");
+        // document.write("<p><strong>Total Cost: " + totalCost + "</strong></p>");
+
+// TAsk 8
+        // var totalMarks = +prompt("Enter Toal Marks");
+        // var marksObtained = +prompt("Enter Obtain Marks");
+
+        // var percentage = (marksObtained / totalMarks) * 100;
+
+        // document.write("<p>Total Marks: " + totalMarks + "</p>");
+        // document.write("<p>Marks Obtained: " + marksObtained + "</p>");
+        // document.write("<p>Percentage: " + percentage.toFixed(2) + "%</p>");
+
+//  Task 9
+// // Define the amount in US Dollars and Saudi Riyals
+//     var usDollars = +prompt("Enter US Dollars");
+//     var Riyals = +prompt("Enter Saudi Riyals");
+//     var usdPak = 104.80;
+//     var RiyalPak = 28;
+//     var totalRupees = (usDollars * usdPak) + (Riyals * RiyalPak);
+//     document.write("<p>Total Currency in Pakistani Rupees: " + totalRupees + " PKR</p>");
+
+// Task 10
+// var initialNumber = 20; 
+
+// var result = ((initialNumber + 5) * 10) / 2;
+// document.write("The result is: " + result);
+
